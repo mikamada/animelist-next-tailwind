9eb24da92295a112289aabdf85897d9604ae7b10
mikamada <mikamada17@gmail.com>
2023-11-12 20:29:23 +0700

    search page

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,18 +1,20 @@
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
+import { getAnimeResponse } from "@/services/api";
 
 const Page = async ({ params }) => {
 	const { keyword } = params;
-	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`
-	);
-	const searchAnime = await response.json();
+	const decodedKeyword = decodeURI(keyword);
+	// const response = await fetch(
+	// 	`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`
+	// );
+	const searchAnime = await getAnimeResponse("anime", `q=${decodedKeyword}`);
 
 	return (
 		<>
 			<section>
 				<Header
-					title={`Pencarian untuk ${keyword}...`}
+					title={`Pencarian untuk ${decodedKeyword}...`}
 					href={"/populer"}
 				/>
 				<AnimeList api={searchAnime} />
