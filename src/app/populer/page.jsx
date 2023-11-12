057f74cfdbb7f75293db9bf93471a0f9e21326f0
mikamada <mikamada17@gmail.com>
2023-11-12 20:29:41 +0700

    popular anime page

--- src/app/populer/page.jsx
+++ src/app/populer/page.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import HeaderMenu from "@/components/Utilities/HeaderMenu";
+import Pagination from "@/components/Utilities/Pagination";
+import AnimeList from "@/components/AnimeList";
+import { getAnimeResponse } from "@/services/api";
 
 const page = () => {
-	return <div>Populer page</div>;
+	const [page, setPage] = useState(1);
+	const [animes, setAnimes] = useState([]);
+
+	const fetchData = async () => {
+		const topAnime = await getAnimeResponse("top/anime", `page=${page}`);
+		setAnimes(topAnime);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, [page]);
+
+	return (
+		<>
+			<HeaderMenu title={`Anime Terpopuler #${page}`} />
+			<AnimeList api={animes} />
+			<Pagination
+				page={page}
+				lastPage={animes.pagination?.last_visible_page}
+				setPage={setPage}
+			/>
+		</>
+	);
 };
 
 export default page;
