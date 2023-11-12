1494909a64f1ab2157acb0fed6457495be0c970d
mikamada <mikamada17@gmail.com>
2023-11-12 21:53:58 +0700

    add recommendationa anime

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,25 @@
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
-import { getAnimeResponse } from "@/services/api";
+import { getAnimeResponse, getNestedAnimeResponse, reproduce } from "@/services/api";
 
 const Page = async () => {
-	
 	const topAnime = await getAnimeResponse("top/anime", "limit=8");
+	let recommendedAnime = await getNestedAnimeResponse(
+		"recommendations/anime",
+		"entry"
+	);
+
+	recommendedAnime = reproduce(recommendedAnime, 10);
 
 	return (
 		<>
-			{/* Top Anime */}
 			<section>
 				<Header title={"Top Anime"} href={"/populer"} label={"Lihat semua"} />
 				<AnimeList api={topAnime} />
+			</section>
+			<section>
+				<Header title={"Recommendation"} />
+				<AnimeList api={recommendedAnime} />
 			</section>
 		</>
 	);
