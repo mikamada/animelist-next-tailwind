2be9bc1ab6f9508fff5d911380717d024abcc32f
mikamada <mikamada17@gmail.com>
2023-11-12 20:30:05 +0700

    home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,17 @@
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
+import { getAnimeResponse } from "@/services/api";
 
 const Page = async () => {
-	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
-	);
-	const respManga = await fetch(`
-	${process.env.NEXT_PUBLIC_API_BASE_URL}/top/manga?limit=10`);
+	
+	const topAnime = await getAnimeResponse("top/anime", "limit=8");
 
-	const topAnime = await response.json();
-	const manga = await respManga.json();
-
-	console.log({manga})
-
-	https: return (
+	return (
 		<>
 			{/* Top Anime */}
 			<section>
 				<Header title={"Top Anime"} href={"/populer"} label={"Lihat semua"} />
 				<AnimeList api={topAnime} />
-			</section>
-
-			{/* Top Manga */}
-			<section>
-				<Header title={"Top Manga"} href={"/new"} label={"Lihat semua"} />
-				<AnimeList api={manga} />
 			</section>
 		</>
 	);
